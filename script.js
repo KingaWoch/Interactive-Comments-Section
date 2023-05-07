8db4accf44e9687156ab78cda937560a25bcaa3e
Kinga Woch <kinga-niecko@o2.pl>
2023-05-07 10:23:44 +0200

    current user

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 fetch("data.json")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    //console.log(data);
     const commentsList = data.comments;
-    console.log(commentsList);
+    //console.log(commentsList);
 
     comments = commentsList.map((comment) => {
-      console.log(comment.content);
+      //console.log(comment.content);
+      console.log(comment.replies[0]);
       return {
         content: comment.content,
         createdAt: comment.createdAt,
         score: comment.score,
         username: comment.user.username,
         avatar: comment.user.image.png,
+        replies: comment.replies,
       };
     });
-    console.log(comments);
-    renderCommentsList(comments);
+
+    const currentUser = data.currentUser;
+    console.log(currentUser.image.png);
+
+    //console.log(comments);
+    renderCommentsList(comments, currentUser);
   });
 
 const createComment = (comment) => {
   // CREATE ELEMENTS
+
+  const commentReplyWrapper = document.createElement("div");
+  commentReplyWrapper.classList.add("comment-and-reply-wrapper");
 
   const commentWrapper = document.createElement("div");
   commentWrapper.classList.add("comment-wrapper");
@@ -75,15 +84,60 @@ const createComment = (comment) => {
 
   commentWrapper.append(commentContent, commentButtons);
 
-  return commentWrapper;
+  commentReplyWrapper.appendChild(commentWrapper);
+
+  // SCORE
+
+  const scoreValue = commentReplyWrapper.querySelector(".score_value");
+
+  const plusBtn = commentReplyWrapper.querySelector(".plus");
+  plusBtn.addEventListener("click", () => {
+    scoreValue.innerText = comment.score++;
+  });
+
+  const minusBtn = commentReplyWrapper.querySelector(".minus");
+  minusBtn.addEventListener("click", () => {
+    if (comment.score < 0) {
+      comment.score === 0;
+    } else {
+      scoreValue.innerText = comment.score--;
+    }
+  });
+
+  return commentReplyWrapper;
 };
 
-const renderCommentsList = (comments) => {
+const renderCommentsList = (comments, currentUser) => {
   const commentsSection = document.querySelector(".comments");
 
   comments.forEach((comment) => {
     commentsSection.appendChild(createComment(comment));
   });
 
+  commentsSection.appendChild(createCurrentUser(currentUser));
+
   return commentsSection;
+};
+
+const createCurrentUser = (currentUser) => {
+  const currentUserWrapper = document.createElement("div");
+  currentUserWrapper.classList.add("current-user-wrapper");
+
+  const currentUserAvatar = document.createElement("img");
+  currentUserAvatar.classList.add("current-user-avatar");
+  console.log(currentUser.image);
+  currentUserAvatar.src = currentUser.image.png;
+
+  const textarea = document.createElement("textarea");
+  textarea.type = "text";
+  textarea.placeholder = "Add a comment...";
+  textarea.classList.add("current-user-comment");
+
+  const sendBtn = document.createElement("button");
+  sendBtn.classList.add("send-btn");
+  sendBtn.innerText = "SEND";
+
+  currentUserWrapper.append(currentUserAvatar, textarea, sendBtn);
+
+  return currentUserWrapper;
 };
